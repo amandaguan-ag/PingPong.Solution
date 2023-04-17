18565e9d9bd7eb1dd205a0d2be28c4486a564b0e
amandaguan-ag <amandaguan1314@gmail.com>
2023-04-16 17:03:17 -0700

    fail CreatPingPongList test

--- PingPong.Tests/ModelTests/NumberTests.cs
+++ PingPong.Tests/ModelTests/NumberTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PingPong.Models;
+using System.Collections.Generic;
 
 namespace PingPong.Tests
 {
@@ -18,5 +19,36 @@ namespace PingPong.Tests
           // Assert
           Assert.AreEqual(typeof(Number), newNumber.GetType());
       }
+      [TestMethod]
+    public void CreatePingPongList_ReturnsPingPongList_ListOfNumbers()
+    {
+        // Arrange
+        int userInput = 15;
+        Number number = new Number(userInput);
+        List<string> expectedList = new List<string>
+        {
+            "1",
+            "2",
+            "ping",
+            "4",
+            "pong",
+            "ping",
+            "7",
+            "8",
+            "ping",
+            "pong",
+            "11",
+            "ping",
+            "13",
+            "14",
+            "ping-pong"
+        };
+    
+        // Act
+        List<string> actualList = number.CreatePingPongList();
+    
+        // Assert
+        CollectionAssert.AreEqual(expectedList, actualList);
+    }
     }
 }
